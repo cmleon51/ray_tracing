0ec287dc221ac6f33f9bd55dbeaa981e9904452a
cmleon51 <cmleon30@gmail.com>
2025-05-20 20:36:20 +0200

    Implemented a BoundingBox for the Plane
    Implemented a BoundingBox for detecting collisions between the ray and a
    plane to consider for a plane's height and width

--- src/world/objects/panel.rs
+++ src/world/objects/panel.rs
@@ -1,6 +1,45 @@
+use std::f64;
+
 use crate::canvas::RGB;
 use crate::world::objects::{Material, Object};
 use crate::world::{Ray, Vec3};
+
+/// object to abstract the bounding box of a Panel
+///
+/// Trough the use of this object we can create a bounding box and check if a specific vector is
+/// inside or outside
+struct BoundingBox {
+    top_right: Vec3,
+    top_left: Vec3,
+    bottom_right: Vec3,
+    bottom_left: Vec3
+}
+
+impl BoundingBox {
+    pub fn new(bounding_center: Vec3, bounding_width: f64, bounding_height: f64) -> Self {
+        return Self {
+            top_right: bounding_center + Vec3::new(bounding_width, bounding_height, 0.0),
+            top_left: bounding_center + Vec3::new(-bounding_width, bounding_height, 0.0),
+            bottom_right: bounding_center + Vec3::new(bounding_width, -bounding_height, 0.0),
+            bottom_left: bounding_center + Vec3::new(-bounding_width, -bounding_height, 0.0),
+        };
+    }
+
+    pub fn is_vec_inside(&self, vec_to_check: Vec3) -> bool {
+        let vec_check_x = *vec_to_check.get_x();
+        let vec_check_y = *vec_to_check.get_y();
+        let vec_check_z = *vec_to_check.get_z();
+
+        return vec_check_x < *self.top_right.get_x()
+            && vec_check_y < *self.top_right.get_y()
+            && vec_check_x > *self.top_left.get_x()
+            && vec_check_y < *self.top_left.get_y()
+            && vec_check_x < *self.bottom_right.get_x()
+            && vec_check_y > *self.bottom_right.get_y()
+            && vec_check_x > *self.bottom_left.get_x()
+            && vec_check_y > *self.bottom_right.get_y();
+    }
+}
 
 /// object to abstract a panel in our ray traced world
 ///
@@ -9,14 +48,16 @@ use crate::world::{Ray, Vec3};
 pub struct Panel {
     panel_origin: Vec3,
     panel_normal: Vec3,
+    bounding_box: BoundingBox,
     material: Material,
 }
 
 impl Panel {
-    pub fn new(panel_origin: Vec3, panel_normal: Vec3, material: Material) -> Self {
+    pub fn new(panel_origin: Vec3, panel_normal: Vec3, panel_width: f64, panel_height: f64, material: Material) -> Self {
         return Self {
             panel_origin,
             panel_normal,
+            bounding_box: BoundingBox::new(panel_origin, panel_width, panel_height),
             material
         };
     }
@@ -32,6 +73,12 @@ impl Object for Panel {
         }
 
         let t = ((self.panel_origin - (*ray.get_direction())).dot_product(&panel_normal)) / discriminant;
+
+        // it's certinately not the best way to check if a ray is inside the width and height of
+        // the box but it's the one i've come up with
+        if !self.bounding_box.is_vec_inside(ray.calculate_ray_position(t)){
+            return None;
+        }
 
         return Some(t);
     }
