c1e6d3a363cfc4da26ad0463bc5152b7ffd10ba1
cmleon51 <cmleon30@gmail.com>
2025-05-25 15:02:04 +0200

    Implemented light attenuation for point lights

--- src/world/lights/point_light.rs
+++ src/world/lights/point_light.rs
@@ -75,7 +75,10 @@ impl Light for PointLight {
                 }
             }
 
-            let mut final_color = (object_color) * light_intensity;
+            // using light_length to determine the light_intensity
+            let attenuation = 1.0 / (1.0 + 0.09 * light_length + 0.032 * light_length * light_length);
+
+            let mut final_color = (object_color) * light_intensity * attenuation;
 
             // calculate the refraction
             if let Some(material_refraction) = *material.get_refraction() {
